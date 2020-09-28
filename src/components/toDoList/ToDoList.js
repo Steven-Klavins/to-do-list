a1d94e162fefc54af49c19b6dbf4ed9570353d43
Graham <grahamfalconer@icloud.com>
2020-09-28 13:12:50 +0100

    removed unwanted files

---
+++ src/components/toDoList/ToDoList.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export default class ToDoList extends Component {
+  render() {
+    return (
+      <div>
+        <h1> To Do List </h1>
+        
+      </div>
+    )
+  }
+}
